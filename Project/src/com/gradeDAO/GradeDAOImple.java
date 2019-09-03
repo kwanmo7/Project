4eb6d775056f8a9dc20111c400ffdc2f520bf53a
hyoeunbae <makimacci@naver.com>
2019-09-03 18:54:37 +0900

    gradeDAOImple

---
+++ Project/src/com/gradeDAO/GradeDAOImple.java
@@ -0,0 +1,8 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Repository("gradeDAO")
+public class GradeDAOImple implements GradeDAO{
+     @Autowired
+	private GradeMapper gradeMapper;
+	
+}
